f94a70ccf6f0e61f30cdb6cbc011c2c07c14cdfd
axelhult <axelhult3@gmail.com>
2017-02-23 15:05:04 +0100

    Will this shit please work?
    Added navbar and backbutton in the filter activity

--- app/src/main/java/com/group11/kth/foreignfriend/FilterActivity.java
+++ app/src/main/java/com/group11/kth/foreignfriend/FilterActivity.java
@@ -1,9 +1,13 @@
 package com.group11.kth.foreignfriend;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,6 +38,52 @@ public class FilterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
+
+
+        // .................. ACTIONS FOR NAVBAR ........................
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    if (item.getItemId()==R.id.profile_id) {
+
+                        Intent intent2;
+                        intent2 = new Intent(getApplicationContext(), ProfileActivity.class);
+                        intent2.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        startActivity(intent2);
+
+                    }
+
+                if (item.getItemId()==R.id.home_id){
+
+                        Intent intent1;
+                        intent1 = new Intent(getApplicationContext(), MapsActivity.class);
+                        intent1.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                        startActivity(intent1);
+                }
+                return false;
+            }
+        });
+
+
+        // ................... ACTION FOR DONE BUTTON ....................
+
+        Button donebutton = (Button) findViewById(R.id.donebtn);
+        donebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Make call to server here??
+
+                Intent intent3 = new Intent (getApplicationContext(), MapsActivity.class);
+                intent3.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(intent3);
+            }
+        });
+
 
         fields = (Button) findViewById(R.id.fieldbtn);
         courses = (Button) findViewById(R.id.coursebtn);
